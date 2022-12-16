5b418fad06e21ce61d409a33e247b3940b7bd523
MinWooJun <76636760+alsdnwns0613@users.noreply.github.com>
2022-12-16 20:48:22 +0900

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,7 @@
+document.getElementById('your-effort').style.display = 'block';
+document.getElementById('calculator').style.display = 'none';
+
+function OpenMain(){
+    document.getElementById('your-effort').style.display = 'none';
+    document.getElementById('calculator').style.display = 'block';
+}
